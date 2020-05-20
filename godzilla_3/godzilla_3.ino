035d061d06d80effdacd7812deb6d1325fd54c7b
Tarin <tarin.eccleston@gmail.com>
2020-05-20 16:56:38 +1200

    Added Fan Servo Motor
    - Added integrated controller and pulsing for servo motor.

--- godzilla_3/godzilla_3.ino
+++ godzilla_3/godzilla_3.ino
@@ -16,7 +16,17 @@
 
 // =================== Pin assignments ====================
 
+// const byte fanServoPin = ;  
+
+Servo fanServo;
+
 // ====================== Variables ======================= 
+
+// Fan Servo Motor
+int minFanServoPulseValue = 900;
+int maxFanServoPulseValue = 2100; 
+float kP_Fan = 0.001f;
+
 
 // ================== Arduino functions ===================
 void setup() {
@@ -24,3 +34,26 @@ void setup() {
 
 void loop() {
 }
+
+void EnableMotors(){
+  motor1.attach(motor1Pin);
+  motor2.attach(motor2Pin);
+  motor3.attach(motor3Pin);
+  motor4.attach(motor4Pin);
+  fanServo.attach(fanServoPin);
+}
+
+void fanServoMotorWrite() {
+    float error = photo1 - photo4; // use phototransistors to allign servo
+    float controlEffort = kP_Fan * error; 
+    float fanServoPulse = Sat2(controlEffort, 400, -400);
+
+    if (fanServoPulse >= 20) {
+        fanServo.writeMicroseconds(1500 + Sat2(fanServoPulse, maxFanServoPulseValue, minFanServoPulseValue));
+    } else
+    {
+        // enter state transition variable here
+    }
+    
+}
+
