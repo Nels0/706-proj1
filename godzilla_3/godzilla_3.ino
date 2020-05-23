c5257cf140c6af402d6c91d25df8d4591c84a6af
Campbell Brown <campbellbrown093@gmail.com>
2020-05-23 13:48:14 +1200

    Added motor functions, tidied up servo control, added helper functions

--- godzilla_3/godzilla_3.ino
+++ godzilla_3/godzilla_3.ino
@@ -12,21 +12,41 @@
  * 
  */
 
+// TODO - implement error for GetServoAngle function
+// TODO - change fanServoPin
+// TODO - change the value for minServoPulseValue
+
 // ======================== Enums =========================
 
 // =================== Pin assignments ====================
-
-// const byte fanServoPin = ;  
-
+//Motors
+const byte motor1Pin = 46;
+const byte motor3Pin = 47;
+const byte motor4Pin = 50;
+const byte motor2Pin = 51;
+const byte fanServoPin = 52; 
+Servo motor1;
+Servo motor2;
+Servo motor3;
+Servo motor4;
 Servo fanServo;
 
 // ====================== Variables ======================= 
 
-// Fan Servo Motor
-int minFanServoPulseValue = 900;
-int maxFanServoPulseValue = 2100; 
-float kP_Fan = 0.001f;
+// Controller gains
+float kP_servoAngle = 0.001f;
+float kI_servoAngle = 0.0f;
+float servoAngle_windup = 10;
 
+// Motors
+float omegaToPulse = 21.3;
+float L1 = 7.5; //distance from centre to front axe
+float L2 = 8.5; //distance from centre to left/right wheen centres
+float Rw = 2.25; //wheel radius in cm
+int maxPulseValue = 250;
+int minPulseValue = 90; 
+int minServoPulseValue = 0;
+int maxServoPulseValue = 600;
 
 // ================== Arduino functions ===================
 void setup() {
@@ -35,7 +55,21 @@ void setup() {
 void loop() {
 }
 
-void EnableMotors(){
+// =================== State machines =========================
+
+// =================== Controllers ============================
+// Servo angle controller
+float GetServoPulse(int deltaTime) {
+  static float I_servoAngle;
+  float error = 0;
+  if(abs(I_servoAngle) < servoAngle_windup){
+    I_servoAngle += error * deltaTime/1000;
+  }
+  return (kP_servoAngle * error) + (kI_servoAngle * I_servoAngle);
+}
+
+// ================== Actuation functions =======================
+void EnableMotors() {
   motor1.attach(motor1Pin);
   motor2.attach(motor2Pin);
   motor3.attach(motor3Pin);
@@ -43,17 +77,46 @@ void EnableMotors(){
   fanServo.attach(fanServoPin);
 }
 
-void fanServoMotorWrite() {
-    float error = photo1 - photo4; // use phototransistors to allign servo
-    float controlEffort = kP_Fan * error; 
-    float fanServoPulse = Sat2(controlEffort, 400, -400);
+void MotorWrite(float Vx, float Vy, float Wz) {
+  float _Vx = Sat2(Vx, 20, 0);
+  float _Vy = Sat2(Vy, 20, 0);
+  float _Wz = Sat2(Wz, 20, 0);
 
-    if (fanServoPulse >= 20) {
-        fanServo.writeMicroseconds(1500 + Sat2(fanServoPulse, maxFanServoPulseValue, minFanServoPulseValue));
-    } else
-    {
-        // enter state transition variable here
-    }
-    
+  // Kinematic equations to convert velocites to motor pulses
+  int motor1Pulse = omegaToPulse * KinematicCalc(_Vx,  _Vy, -_Wz);
+  int motor2Pulse = omegaToPulse * KinematicCalc(_Vx, -_Vy,  _Wz);
+  int motor3Pulse = omegaToPulse * KinematicCalc(_Vx, -_Vy, -_Wz);
+  int motor4Pulse = omegaToPulse * KinematicCalc(_Vx,  _Vy,  _Wz);
+
+  motor1.writeMicroseconds(1500 + Sat2(motor1Pulse, maxPulseValue, minPulseValue));
+  motor2.writeMicroseconds(1500 - Sat2(motor2Pulse, maxPulseValue, minPulseValue));
+  motor3.writeMicroseconds(1500 + Sat2(motor3Pulse, maxPulseValue, minPulseValue));
+  motor4.writeMicroseconds(1500 - Sat2(motor4Pulse, maxPulseValue, minPulseValue));
 }
 
+void ServoWrite(int servoPulse) {
+  fanServo.writeMicroseconds(1500 + Sat2(servoPulse, maxServoPulseValue, minServoPulseValue));
+}
+
+// ====================== Helper functions ===========================
+int Sat2(int value, int maxValue, int minValue) {
+  // Will clamp the value if the value is outside the maximum ranges
+  if (value < -maxValue)
+    return -maxValue;
+  else if (value > maxValue)
+    return maxValue;
+  else
+    return value;
+
+  // Will clamp if the value is too close to zero
+  if (value < minValue)
+    return 0;
+  else if (value > -minValue) 
+    return 0;
+  else 
+    return value; 
+}
+
+int KinematicCalc(int Vx, int Vy, int Wz) {
+  return (int)((Vx + Vy + Wz*(L1 + L2)) / Rw);
+}
